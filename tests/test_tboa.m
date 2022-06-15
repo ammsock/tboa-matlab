e1e948a94931ee28220174ba7946930020e119ca
Jörn Dietrich <joern.dietrich@uni-ulm.de>
2022-06-15 13:55:22 +0200

    Adds Copyright note and GPLv3 header

--- tests/test_tboa.m
+++ tests/test_tboa.m
@@ -1,3 +1,16 @@
+%  Copyright (C) 2022 Jörn Dietrich
+%  ----------------------------------------------------------------------------
+% This file is part of AMMSoCK.
+%
+% AMMSoCK is free software: you can redistribute it and/or modify it under the
+% terms of the GNU General Public License as published by the Free Software
+% Foundation, either version 3 of the License, or (at your option) any later
+% version. AMMSoCK is distributed in the hope that it will be useful, but
+% WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+% FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+% details. You should have received a copy of the GNU General Public License
+% along with AMMSoCK. If not, see <https://www.gnu.org/licenses/>.
+
 close all
 clear all
 
@@ -6,9 +19,9 @@ tspan = [zeros(size(tf)),tf];
 ntf = size(tspan,1);
 
 % selected_model = 'linear';
-% selected_model = 'davis_skodje';
+selected_model = 'davis_skodje';
 % selected_model = 'michaelis_menten';
-selected_model = 'center_manifold';
+% selected_model = 'center_manifold';
 
 if strcmp(selected_model,'linear')
   gamma = 5;
